9e7415ff90a3558dc6d1441fcc8583f04b62b03e
martianrock <martianrock@users.noreply.github.com>
2018-02-21 20:15:17 -0500

    Update crontab_gen.sh

--- scripts/crontab_gen.sh
+++ scripts/crontab_gen.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #set -e
 
-echo "${CRON_RUN_MINUTE-0} ${CRON_RUN_HOUR-0} * * * /pgpass.sh /pg_backup_rotate.sh" >/etc/crontabs/root
+echo "${CRON_RUN_MINUTE-0} ${CRON_RUN_HOUR-0} * * * /pgpass.sh /pg_backup_rotated.sh" >/etc/crontabs/root
